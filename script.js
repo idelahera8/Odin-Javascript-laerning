71118006cf9dd0f270a1f0535ec1a8a27d138825
idelahera <idelahera8@gmail.com>
2022-05-30 11:01:51 +0200

    First 2 functions!

---
+++ script.js
@@ -0,0 +1,15 @@
+function add7() {
+    let num = parseInt(prompt("Select a number"))
+    let result = num + 7
+    console.log(result)
+}
+
+function multiply() {
+    let num1 = parseInt(prompt("Select a number"))
+    let num2 = parseInt(prompt("Select a number"))
+    result = num1 * num2
+    console.log(result)
+}
+
+add7()
+multiply()
